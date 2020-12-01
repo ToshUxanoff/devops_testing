bea7b121c1c7930a8aacd68c6a6730ec2ad78860
ToshUxanoff <toshu.uhanoff@yandex.ru>
2020-12-01 17:14:11 +0300

    added database

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import psycopg2
 
+conn = psycopg2.connect(database="test", user="test", password="root", host="postgres", port=5432)
 class Handler(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
@@ -8,13 +10,18 @@ class Handler(BaseHTTPRequestHandler):
         
     def do_GET(self):
         self._set_response()
+        curr = conn.cursor()
+        curr.execute("SELECT * FROM requests")
+        data = curr.fetchall()
         print("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write("GET request for {}. Data is: {}".format(self.path, data).encode('utf-8'))
         
     def do_POST(self):
         data = self.rfile.read(int(self.headers['Content-Length']))
         self._set_response()
-        data = data.decode('utf-8')
+        data = str(data.decode('utf-8'))
+        curr = conn.cursor()
+        curr.execute("INSERT INTO requests (data) VALUES (%s)", (data,))
         print("POST request: {}, {}".format(self.path, data))
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
         
